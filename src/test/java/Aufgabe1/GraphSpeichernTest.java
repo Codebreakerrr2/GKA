fa2a8dd74b08bb005ee13a5f70ef10dc208eac2e
Codebreakerrr2 <144541673+Codebreakerrr2@users.noreply.github.com>
2024-05-08 16:02:31 +0200

    saveTest sind da

--- src/test/java/Aufgabe1/GraphSpeichernTest.java
+++ src/test/java/Aufgabe1/GraphSpeichernTest.java
@@ -1,5 +1,7 @@
 package Aufgabe1;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.MultiGraph;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +9,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class GraphSpeichernTest {
 
     @Test
-    void saveGraphToFile4() {
+    void leererGraphSaveTest() {
+        Graph graph= new MultiGraph("1");
+        String path="C:\\Users\\Usman\\Documents\\Java Files\\GKA\\src\\main\\java\\Aufgabe1\\graphDateien\\leererGraphSaveTest";
+        GraphSpeichern.saveGraphToFile4(graph,path);
+    }
+
+    @Test
+    void directedGraphSaveTest() {
+        Graph graph= GraphLesen.readGraph("C:\\Users\\Usman\\Documents\\Java Files\\GKA\\src\\main\\java\\Aufgabe1\\Dateien_1_gka\\graph01.gka");
+        String path="C:\\Users\\Usman\\Documents\\Java Files\\GKA\\src\\main\\java\\Aufgabe1\\graphDateien\\directedGraphSaveTest";
+        GraphSpeichern.saveGraphToFile4(graph,path);
+
+
+
+    }
+    @Test
+    void UndirectedGraphSaveTest() {
+
+        Graph graph= GraphLesen.readGraph("C:\\Users\\Usman\\Documents\\Java Files\\GKA\\src\\main\\java\\Aufgabe1\\Dateien_1_gka\\graph02.gka");
+        String path="C:\\Users\\Usman\\Documents\\Java Files\\GKA\\src\\main\\java\\Aufgabe1\\graphDateien\\UndirectedGraphSaveTest";
+        GraphSpeichern.saveGraphToFile4(graph,path);
+
+
     }
 }
